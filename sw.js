3600f1eadd3f73602c1f0c77aefd828d56fe0e20
realgheart-art <realgheart@gmail.com>
2026-08-09 08:14:43 +0200

    Update sw3.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker — Dialog Prestasi PPD
-const CACHE = "dialog-prestasi-v2";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const CACHE = "dialog-prestasi-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./logo.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
